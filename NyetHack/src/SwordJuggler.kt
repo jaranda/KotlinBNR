b01860ad1bfb56493d5afa69cfad803babd861a7
Juan Aranda <jaranda@a3nstudio.com>
2020-12-31 15:20:32 -0600

    Chapter 6 - Adding a try/catch statement.

--- NyetHack/src/SwordJuggler.kt
+++ NyetHack/src/SwordJuggler.kt
@@ -5,14 +5,18 @@ fun main(Args: Array<String>) {
         swordsJuggling = 2
     }
 
-    proficiencyCheck(swordsJuggling)
-    swordsJuggling = swordsJuggling!!.plus(1)
-
+    try {
+        proficiencyCheck(swordsJuggling)
+        swordsJuggling = swordsJuggling!!.plus(1)
+    } catch (e: Exception) {
+        println(e)
+    }
+    
     println("You juggle $swordsJuggling swords!")
 }
 
 fun proficiencyCheck(swordsJuggling: Int?) {
-    swordsJuggling ?: throw IllegalStateException("Player cannont Juggle swords")
+    swordsJuggling ?: throw UnskilledSwordJugglerException()
 }
 
 class UnskilledSwordJugglerException() : IllegalStateException("Player cannot juggle swords")
