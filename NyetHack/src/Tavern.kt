4298fc2a8e5cdb1926df3a3148a35a0a6fefe058
Juan Aranda <jaranda@a3nstudio.com>
2020-12-31 14:01:12 -0600

    Chapter 6 - Using let with the safe call operator.

--- NyetHack/src/Tavern.kt
+++ NyetHack/src/Tavern.kt
@@ -1,5 +1,11 @@
 fun main(Args: Array<String>) {
-    var beverage = readLine()?.capitalize()
+    var beverage = readLine()?.let {
+        if (it.isNotBlank()) {
+            it.capitalize()
+        } else {
+            "Buttered Ale"
+        }
+    }
 //    beverage = null
 
     print(beverage)
